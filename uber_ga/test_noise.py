38bb97d41bef6b9b68470f25d667a56bd0b95bcd
Alex Nichol <unixpickle@gmail.com>
2017-12-21 12:00:29 -0800

    basic test for noise cache

--- uber_ga/test_noise.py
+++ uber_ga/test_noise.py
@@ -24,6 +24,19 @@ def test_noise_determinism():
     source1 = NoiseSource(seed=124, size=(1 << 24))
     assert not np.allclose(source1.block(15, 2), source.block(15, 2))
 
+def test_noise_cumulative():
+    """
+    Test that cumulative_block() gives correct data.
+    """
+    source = NoiseSource()
+    genome = [(1337, 0.5), (123, 0.7), (123123, 1), (3213221, 0.1)]
+    expected = np.sum([source.block(15, seed) * scale for seed, scale in genome], axis=0)
+    actual = source.cumulative_block(15, genome)
+    actual1 = source.cumulative_block(15, genome)
+    assert actual.shape == (15,)
+    assert np.allclose(actual, actual1)
+    assert np.allclose(actual, expected)
+
 def test_noise_performance(benchmark):
     """
     Benchmark noise generation.
